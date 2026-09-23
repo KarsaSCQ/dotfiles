71c5d6db1a7515ed032238bb78656b0e0711c7df
KarsaSCQ <pgm_scq@163.com>
2026-09-23 13:58:22 +0800

    fix: add missing -s flag to chsh command
    Without -s, chsh enters interactive mode and block the script from continuing unattended.

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ cp ~/dotfiles/.zshrc ~/.zshrc
 cp ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 cp -r ~/dotfiles/omz-custom/. ~/.oh-my-zsh/custom/
 
-chsh -s "$(which zsh)"
+# 设定zsh为默认shell
+chsh -s  "$(which zsh)"
 
 echo "✅ 安装完成！请退出终端重新登录，或者直接执行 exec zsh 查看效果"
